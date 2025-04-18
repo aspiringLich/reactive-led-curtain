f55a6b7d9584c85d63e042fd09775c11a40523aa
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-04-18 14:30:11 -0400

    piano roll sort of

--- lib/src/state/paint.rs
+++ lib/src/state/paint.rs
@@ -1,11 +1,11 @@
 use std::iter;
 
 use ecolor::Color32;
-use tiny_skia::{Color, Paint, Pixmap, Point, Rect, Shader, SpreadMode, Transform};
+use tiny_skia::{Color, Pixmap};
 
 use crate::{
     cfg::AnalysisConfig,
-    color::{Oklch, OklchGradient, OklchGradientStop},
+    color::{Oklch, OklchGradient},
     easing::EasingFunctions,
     util::profile_function,
 };
@@ -16,6 +16,13 @@ use super::{light::LightData, power::PowerData};
 pub struct PaintData {
     pub colors: Vec<Color32>,
     pub pix: Pixmap,
+    pub harmonic: Canvas<HarmonicPixel>,
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct HarmonicPixel {
+    pub color: f32,
+    pub intensity: f32,
 }
 
 struct PaintCtx<'a> {
@@ -24,17 +31,6 @@ struct PaintCtx<'a> {
     power: &'a PowerData,
     w: f32,
     h: f32,
-    center_top: Point,
-    center_bottom: Point,
-    full_rect: Rect,
-}
-
-impl<'a> PaintCtx<'a> {
-    fn vrect(&self, x: f32) -> Rect {
-        let w = self.w;
-        let h = self.h;
-        Rect::from_ltrb(x, 0.0, x + 1.0, h).unwrap()
-    }
 }
 
 impl PaintData {
@@ -45,6 +41,7 @@ impl PaintData {
                 Color32::BLACK,
                 cfg.light.width as usize * cfg.light.height as usize,
             )),
+            harmonic: Canvas::new(cfg, HarmonicPixel::default()),
         }
     }
 
@@ -56,9 +53,6 @@ impl PaintData {
     ) -> PaintCtx<'a> {
         let w = self.pix.width() as f32;
         let h = self.pix.height() as f32;
-        let center_top = Point::from_xy(w / 2.0, 0.0);
-        let center_bottom = Point::from_xy(w / 2.0, h);
-        let full_rect = Rect::from_ltrb(0.0, 0.0, w, h).unwrap();
 
         PaintCtx {
             easing,
@@ -66,9 +60,6 @@ impl PaintData {
             power,
             w,
             h,
-            center_top,
-            center_bottom,
-            full_rect,
         }
     }
 
@@ -90,11 +81,11 @@ impl PaintData {
         self
     }
 
-    fn percussive_background<'a>(&mut self, ctx: &mut PaintCtx<'a>) -> Canvas {
+    fn percussive_background<'a>(&mut self, ctx: &mut PaintCtx<'a>) -> Canvas<Oklch> {
         let p = ctx.light.percussive.average();
         let b = ctx.light.bass_percussive.average();
 
-        let mut canvas = Canvas::new(ctx);
+        let mut canvas = Canvas::new(ctx, Oklch::TRANSPARENT);
 
         const FACTOR: f32 = 0.4;
         let ratio = p / (p + b + f32::EPSILON) * FACTOR * FACTOR;
@@ -114,37 +105,79 @@ impl PaintData {
         canvas
     }
 
-    fn harmonic_lines<'a>(&mut self, ctx: &mut PaintCtx<'a>, canvas: &mut Canvas) {
+    fn harmonic_lines<'a>(&mut self, ctx: &mut PaintCtx<'a>, canvas: &mut Canvas<Oklch>) {
         // let mut paint = Paint::default();
         let padding = ((ctx.w - 12.0) / 2.0) as usize;
         let grad = OklchGradient::new_hex(["#ff0d17", "#ecaf3b", "#6cd74a"].into_iter());
 
-        for (i, row) in canvas.iter_rows().enumerate() {
-            for j in 0..12 {
-                let o = ctx.easing.note.ease_normalize(ctx.light.notes[j].average()) * 0.9;
-                let avg = ctx
-                    .easing
-                    .octave
-                    .ease_normalize(ctx.power.average_octave[j]);
-                let color = grad.color(avg).unwrap();
-                row[j + padding] = row[j + padding].lerp(&color, o);
+        self.harmonic.rotate_down();
+        for j in 0..12 {
+            let intensity = ctx.easing.note.ease_normalize(ctx.light.notes[j].average()) * 0.9;
+            let color = ctx
+                .easing
+                .octave
+                .ease_normalize(ctx.power.average_octave[j]);
+            // let color = grad.color(avg).unwrap();
+            // row[j + padding] = row[j + padding].lerp(&color, o);
+            self.harmonic.row(0)[j + padding] = HarmonicPixel { color, intensity };
 
-                // ( ͡° ͜ʖ ͡°)
-                let edge_factor = 0.5;
-                if j < padding {
-                    row[padding + 12 + j] = row[padding + 12 + j].lerp(
-                        &color,
-                        o * (padding - j) as f32 / padding as f32 * edge_factor,
-                    );
+            // ( ͡° ͜ʖ ͡°)
+            let edge_factor = 0.5;
+            if j < padding {
+                self.harmonic.row(0)[padding + 12 + j] = HarmonicPixel {
+                    color,
+                    intensity: intensity * (padding - j) as f32 / padding as f32 * edge_factor,
                 }
-                if j >= 12 - padding {
-                    row[j + padding - 12] = row[j + padding - 12].lerp(
-                        &color,
-                        o * (j + padding - 12) as f32 / padding as f32 * edge_factor,
-                    );
+            }
+            if j >= 12 - padding {
+                self.harmonic.row(0)[padding + j - 12] = HarmonicPixel {
+                    color,
+                    intensity: intensity * (j + padding - 12) as f32 / padding as f32 * edge_factor,
                 }
             }
         }
+
+        for (rowh, rowc) in iter::repeat_n(self.harmonic.row_immut(0), 12)
+            .chain(iter::repeat_n(self.harmonic.row_immut(1), 5))
+            .chain(iter::repeat_n(self.harmonic.row_immut(2), 3))
+            .chain(iter::repeat_n(self.harmonic.row_immut(3), 2))
+            .chain(iter::repeat_n(self.harmonic.row_immut(4), 2))
+            .chain(iter::repeat_n(self.harmonic.row_immut(5), 1))
+            .chain(iter::repeat_n(self.harmonic.row_immut(6), 1))
+            .chain(iter::repeat_n(self.harmonic.row_immut(7), 1))
+            .zip(canvas.iter_rows())
+        {
+            for (pixelh, pixelc) in rowh.iter().zip(rowc.iter_mut()) {
+                *pixelc = pixelc.lerp(&grad.color(pixelh.color).unwrap(), pixelh.intensity)
+            }
+        }
+
+        // for (i, row) in canvas.iter_rows().enumerate() {
+        //     for j in 0..12 {
+        //         let o = ctx.easing.note.ease_normalize(ctx.light.notes[j].average()) * 0.9;
+        //         let avg = ctx
+        //             .easing
+        //             .octave
+        //             .ease_normalize(ctx.power.average_octave[j]);
+        //         let color = grad.color(avg).unwrap();
+        //         row[j + padding] = row[j + padding].lerp(&color, o);
+
+        //         // ( ͡° ͜ʖ ͡°)
+        //         let edge_factor = 0.5;
+        //         if j < padding {
+        //             row[padding + 12 + j] = row[padding + 12 + j].lerp(
+        //                 &color,
+        //                 o * (padding - j) as f32 / padding as f32 * edge_factor,
+        //             );
+        //         }
+        //         if j >= 12 - padding {
+        //             row[j + padding - 12] = row[j + padding - 12].lerp(
+        //                 &color,
+        //                 o * (j + padding - 12) as f32 / padding as f32 * edge_factor,
+        //             );
+        //         }
+        //     }
+        // }
     }
 }
 
@@ -159,30 +192,68 @@ impl Methods for Color32 {
 }
 
 #[derive(Clone, Debug)]
-pub struct Canvas {
-    data: Vec<Oklch>,
+pub struct Canvas<T> {
+    data: Vec<T>,
     w: u32,
     h: u32,
 }
 
-impl Canvas {
-    fn new<'a>(ctx: &PaintCtx<'a>) -> Self {
-        let w = ctx.w;
-        let h = ctx.h;
-        let data = Vec::from_iter(iter::repeat_n(Oklch::TRANSPARENT, w as usize * h as usize));
-        Self {
-            data,
-            w: w as u32,
-            h: h as u32,
-        }
+trait CanvasWidthHeight {
+    fn width(&self) -> u32;
+    fn height(&self) -> u32;
+}
+
+impl<'a> CanvasWidthHeight for PaintCtx<'a> {
+    fn width(&self) -> u32 {
+        self.w as u32
+    }
+    fn height(&self) -> u32 {
+        self.h as u32
+    }
+}
+
+impl CanvasWidthHeight for AnalysisConfig {
+    fn width(&self) -> u32 {
+        self.light.width
+    }
+    fn height(&self) -> u32 {
+        self.light.height
+    }
+}
+
+impl<T> Canvas<T> {
+    fn new<'a>(ctx: &impl CanvasWidthHeight, default: T) -> Self
+    where
+        T: Clone,
+    {
+        let w = ctx.width();
+        let h = ctx.height();
+        let data = Vec::from_iter(iter::repeat_n(default, w as usize * h as usize));
+        Self { data, w, h }
     }
 
-    pub fn row(&mut self, r: usize) -> &mut [Oklch] {
+    pub fn row(&mut self, r: usize) -> &mut [T] {
         let start = r * self.w as usize;
         let end = start + self.w as usize;
         &mut self.data[start..end]
     }
 
+    pub fn row_immut(&self, r: usize) -> &[T] {
+        let start = r * self.w as usize;
+        let end = start + self.w as usize;
+        &self.data[start..end]
+    }
+
+    pub fn iter_rows(&mut self) -> impl ExactSizeIterator<Item = &mut [T]> {
+        self.data.chunks_mut(self.w as usize)
+    }
+
+    pub fn rotate_down(&mut self) {
+        self.data.rotate_right(self.w as usize);
+    }
+}
+
+impl Canvas<Oklch> {
     pub fn overlay(&mut self, other: &Self) {
         for i in 0..self.data.len() {
             self.data[i] = self.data[i].overlay(&other.data[i]);
@@ -201,9 +272,5 @@ impl Canvas {
                 )
             })
             .collect()
-    }
-
-    pub fn iter_rows(&mut self) -> impl ExactSizeIterator<Item = &mut [Oklch]> {
-        self.data.chunks_mut(self.w as usize)
     }
 }
